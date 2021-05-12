5169cca6b35726546a929e46a8a7567f5f058eb9
mohira <shukatsudaison@gmail.com>
2021-05-12 15:14:43 +0900

    p.009: RED: lexer.Next()のテストケースの拡張 / 「識別子」「キーワード」「数」を追加

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -6,21 +6,66 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+}
+
+let result = add(five, ten);
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		// 1行目 let five = 5;
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
+
+		// 2行目 let ten = 10;
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		// 4行目(3行目は空行なので無関係) let add = fn(x, y) {
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "}"},
+
+		// 5行目 x + y;
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+
+		// 6行目 }
+		{token.RBRACE, "}"},
+
+		// 8行目(7行目は空行なので無関係) let result = add(five, ten);
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
 	}
 
 	l := New(input)
