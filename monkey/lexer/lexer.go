ef60f28f89478e8cbbf742896fd191ca3750ff53
mohira <shukatsudaison@gmail.com>
2021-05-12 14:58:05 +0900

    p.007: コンパイルエラー: lexer.readChar()の実装

--- monkey/lexer/lexer.go
+++ monkey/lexer/lexer.go
@@ -9,5 +9,26 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+// 次の1文字を読んでinput文字列の現在位置をすすめる
+// 注意: ASCII文字対応のみでUnicodeには非対応。バイト列の解析が必要になるからね(詳しくはp.7参照))。
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		// 入力の終端チェック(読み切った場合)
+
+		// byte(0) は ASCIIコードの "NUL"文字
+		// https://play.golang.org/p/6NnGcUgwNBt
+		l.ch = 0
+	} else {
+		// 次の文字を読み込む
+		l.ch = l.input[l.readPosition]
+	}
+
+	// 検査対象の文字の位置を進める
+	// lo.positionは常に最後に読んだ場所を指し示す
+	l.position = l.readPosition
+	l.readPosition += 1
 }
