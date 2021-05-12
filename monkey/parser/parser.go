90d71106beea8ccdfda1ed2810bd1d9bfec97532
mohira <shukatsudaison@gmail.com>
2021-05-12 18:31:38 +0900

    p.041: GREEN: let文を構文解析できるようになった！

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -50,3 +50,58 @@ func (p *Parser) ParseProgram() *ast.Program {
 
 	return program
 }
+
+// プログラムが読んでいるトークンにあわせて構文を解析していく
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() ast.Statement {
+	// let文は
+	// 		let <identifier> = <expression>;
+	// という構造なので、 let → Identifier → ASSIGN → Expression → SEMICOLON と期待していく
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	//
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	// p.expectPeek() の次のトークンに進めているので、ここの curToken は 識別子 が入っている
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// 構文解析器のよくある「アサーション関数」らしい
+// 次のトークンの型が期待されるものだったときだけトークンを進める
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
