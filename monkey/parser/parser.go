73b3101e6aab5c8053f13d80d15465d15f516795
mohira <shukatsudaison@gmail.com>
2021-05-12 18:17:09 +0900

    p.039: コンパイルエラー: program.parseProgram()

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -34,5 +34,19 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{} // ASTのルートノードの生成
+	program.Statements = []ast.Statement{}
+
+	// EOFになるまで「トークン」を読み続ける
+	// 構文を解析しては、Statementとして溜め込んでいく
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
 }
