64de279b8f0d75d4914beb13d1a2e96c71995198
mohira <shukatsudaison@gmail.com>
2021-05-15 15:59:57 +0900

    p.056: RED: 最もシンプルな式である識別子の構文解析のテストを追加

--- monkey/parser/parser_test.go
+++ monkey/parser/parser_test.go
@@ -115,3 +115,40 @@ func checkParserErrors(t *testing.T, p *Parser) {
 
 	t.FailNow()
 }
+
+func TestIdentifierExpression(t *testing.T) {
+	// 識別子は式。
+	// 他のあらゆる式と同様に、識別子は値を生成する
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	// input は foobar; しか入ってないからね
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+	// foobar; は 式だから、ExpressionStatementじゃなきゃ困る
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier. got=%T", stmt.Expression)
+	}
+
+	if ident.Value != "foobar" {
+		t.Errorf("ident.Value not %s. got=%s", "foobar", ident.Value)
+	}
+
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", "foobar", ident.TokenLiteral())
+	}
+}
