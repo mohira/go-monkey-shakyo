25cb5ef5e9f0f3bbca2d4db10792ae68ffd649af
mohira <shukatsudaison@gmail.com>
2021-05-15 15:30:56 +0900

    p.052: GREEN: ExpressionStatementを追加

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -65,3 +65,14 @@ func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es ExpressionStatement) statementNode() {}
+
+func (es ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
