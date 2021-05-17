ebf5910eb56156ddcd903a06f66eeef90b0aa04a
mohira <shukatsudaison@gmail.com>
2021-05-17 13:24:08 +0900

    p.094 GREEN: ブロック文の構造体を定義。ブロック文は1つの文ではなく文の集合であることに注意。

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -244,3 +244,24 @@ func (ie *IfExpression) TokenLiteral() string {
 
 	return out.String()
 }
+
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode() {}
+
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, statement := range bs.Statements {
+		out.WriteString(statement.String())
+	}
+
+	return out.String()
+}
