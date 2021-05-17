226d193b6af3e1544ca8a50d971b3f15ec897a5d
mohira <shukatsudaison@gmail.com>
2021-05-17 15:45:41 +0900

    p.100 GREEN: 関数リテラルの定義

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"go-monkey-shakyo/monkey/token"
+	"strings"
 )
 
 type Node interface {
@@ -262,6 +263,37 @@ func (bs *BlockStatement) String() string {
 	for _, statement := range bs.Statements {
 		out.WriteString(statement.String())
 	}
+
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token // 'fn' トークン
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+// 関数リテラルの中核となるのは、パラメータのリストと関数本体のブロック文
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
