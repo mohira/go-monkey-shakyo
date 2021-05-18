97cb066e93124acc351e61740607e17eb6375bda
mohira <shukatsudaison@gmail.com>
2021-05-18 16:18:18 +0900

    p.112 GREEN: RLPL を作った！ 楽しい！

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"go-monkey-shakyo/monkey/lexer"
-	"go-monkey-shakyo/monkey/token"
+	"go-monkey-shakyo/monkey/parser"
 	"io"
 )
 
@@ -23,9 +23,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserError(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserError(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
