7591297f254eba9dfd5033a9b09da7a2db835023
mohira <shukatsudaison@gmail.com>
2021-05-18 22:43:25 +0900

    p.127 GREEN Eval()においてASTをたどるようにした。そして、整数リテラルだけを含む式文の評価ができるようになった！
    - 式文の理解が怪しいことに気づいた
    - p.51 式文は実際には厳然たる文というわけではなく、1つの式だけからなる文のことだ。

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -7,8 +7,25 @@ import (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+
+	// 文の場合
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// 式の場合
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
 	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+	return result
 }
