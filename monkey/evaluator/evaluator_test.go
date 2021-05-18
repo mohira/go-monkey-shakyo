f5fdb6b01904416fcbfb8af04cf139d752829509
mohira <shukatsudaison@gmail.com>
2021-05-18 22:21:16 +0900

    p.125 コンパイルエラー 整数リテラルだけを含む式文が与えられたときに、それを評価すると、その整数そのものが返ってくるテストを追加

---
+++ monkey/evaluator/evaluator_test.go
@@ -0,0 +1,52 @@
+package evaluator
+
+import (
+	"go-monkey-shakyo/monkey/lexer"
+	"go-monkey-shakyo/monkey/object"
+	"go-monkey-shakyo/monkey/parser"
+	"testing"
+)
+
+// 整数リテラルだけを含む式文が与えられたときに、それを評価すると、その整数そのものが返ってくる
+// 要は、「5を打ち込むと、5が返ってくる」
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected int64
+	}{
+		{"5 を評価したら 5 になる", "5", 5},
+		{"10 を評価したら 10 になる", "10", 10},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			evaluated := testEval(tt.input)
+
+			testIntegerObject(t, evaluated, tt.expected)
+		})
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
