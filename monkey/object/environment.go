3db0a436796579105e3981cc634780bc1a44fe3e
mohira <shukatsudaison@gmail.com>
2021-05-21 14:42:31 +0900

    p166: RED 関数呼び出しの引数を関数本体からアクセスできるようにするために、「環境の拡張」をできるようにした。具体的には、ある環境のGetメソッドが呼ばれたときに、その環境内で関連した名前が見つからない場合には、「外の環境」(ある環境を包んでいる側))で探しにいく。これを繰り返す。

--- monkey/object/environment.go
+++ monkey/object/environment.go
@@ -7,14 +7,30 @@ func NewEnvironment() *Environment {
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+
+	// 現在の「環境」には存在しないが、「外の環境」にはあるかもしれないので探しに行く
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
+
 	return obj, ok
 }
 
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
+}
+
+// "enclosed" の意味を理解するとイメージできる
+// http://ejbridge2.blog.fc2.com/blog-entry-146.html
+// https://res.cloudinary.com/dyd911kmh/image/upload/f_auto,q_auto:best/v1588956604/Scope_fbrzcw.png
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
