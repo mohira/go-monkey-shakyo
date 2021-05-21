3286517a15003eb7aac3dd6f92a0ae48136617d6
mohira <shukatsudaison@gmail.com>
2021-05-21 14:04:59 +0900

    p.162: RED 関数リテラルを評価したときに、正しいパラメータと本文を持った*object.Functionが返されるかのテストを追加。関数は「値」であって、「文」ではない。だから、内部表現(=オブジェクト)のテストをしている。

--- monkey/evaluator/evaluator_test.go
+++ monkey/evaluator/evaluator_test.go
@@ -317,3 +317,28 @@ func TestLetStatements(t *testing.T) {
 		})
 	}
 }
+
+// 関数リテラルを評価したときに、正しいパラメータと本文を持った*object.Functionが返されるかのテスト
+func TestFunctionObject(t *testing.T) {
+	input := `fn(x) { x + 2; };`
+
+	evaluated := testEval(input)
+
+	fn, ok := evaluated.(*object.Function)
+	if !ok {
+		t.Fatalf("object is not Function. got=%T(%+v)", evaluated, evaluated)
+	}
+
+	if len(fn.Parameters) != 1 {
+		t.Fatalf("function has wrong parameters. Paramters=%+v", fn.Parameters)
+	}
+
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("parameter is not 'x'. got=%q", fn.Parameters[0])
+	}
+
+	expectedBody := "(x + 2)"
+	if fn.Body.String() != expectedBody {
+		t.Fatalf("body in not %q. got=%q", expectedBody, fn.Body.String())
+	}
+}
