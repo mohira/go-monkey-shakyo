aca0dfb34ecd480b4cef5e1f5dd94c6430833c2a
mohira <shukatsudaison@gmail.com>
2021-05-22 18:59:15 +0900

    p.187: RED まずは 組み込み関数len()を見つけられるようにするために、組み込み関数のための環境にlenを定義し、識別子の評価時に組み込み関数の環境を探しに行くようにした。
    ```
    $ go run main.go
    >> len;
    builtin function
    >> len();
    ERROR: not a function: BUILTIN
    ```

---
+++ monkey/evaluator/builtins.go
@@ -0,0 +1,10 @@
+package evaluator
+
+import "go-monkey-shakyo/monkey/object"
+
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			return NULL
+		}},
+}
