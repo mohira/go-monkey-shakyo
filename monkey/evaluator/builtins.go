124aa8808dda8bed9438a53a225efa7ba193c4c0
mohira <shukatsudaison@gmail.com>
2021-05-22 19:14:19 +0900

    p.189: GREEN 組み込み関数len()の実装完了
    ```
    $ go run main.go
    >> len("hello")
    5
    >> len("")
    0
    >> len(1)
    ERROR: argument to `len` not supported, got INTEGER
    >> len("foo", "bar")
    ERROR: wrong number of arguments. got=2, want=1
    ```

--- monkey/evaluator/builtins.go
+++ monkey/evaluator/builtins.go
@@ -1,10 +1,23 @@
 package evaluator
 
-import "go-monkey-shakyo/monkey/object"
+import (
+	"fmt"
+	"go-monkey-shakyo/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
-			return NULL
+			if len(args) != 1 {
+				return &object.Error{Message: fmt.Sprintf("wrong number of arguments. got=%d, want=1", len(args))}
+			}
+
+			// ifで分岐するより、switchで式を定義したほうがスッキリ書けるね
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return &object.Error{Message: fmt.Sprintf("argument to `len` not supported, got %s", args[0].Type())}
+			}
 		}},
 }
