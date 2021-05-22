6d6367d647d6b75eb0fc1852c31ee02fcfb2e759
mohira <shukatsudaison@gmail.com>
2021-05-22 18:04:23 +0900

    p.177 RED STRINGトークンの追加と、字句解析器がSTRINGトークンに対応できているかのテストを追加

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -25,6 +25,8 @@ if (5 < 10) {
 
 10 == 10;
 10 != 9;
+"foobar"
+"foo bar"
 `
 
 	tests := []struct {
@@ -132,6 +134,12 @@ if (5 < 10) {
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+
+		// 「"」も含めて1つのトークンとして扱う
+		// "foobar"; と "foo bar";
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
+
 		{token.EOF, ""},
 	}
 
