151aa1cdc70c6dedf57033986d41230085a62e5f
mohira <shukatsudaison@gmail.com>
2021-05-26 11:12:16 +0900

    p.206: GREEN 組み込み関数first()を実装

--- monkey/evaluator/builtins.go
+++ monkey/evaluator/builtins.go
@@ -21,4 +21,22 @@ var builtins = map[string]*object.Builtin{
 				return &object.Error{Message: fmt.Sprintf("argument to `len` not supported, got %s", args[0].Type())}
 			}
 		}},
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return NULL
+		},
+	},
 }
