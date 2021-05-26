cfe8f8d854e6a70ed791d6dd00298df7630e07ce
mohira <shukatsudaison@gmail.com>
2021-05-26 11:16:26 +0900

    p.206: GREEN 組み込み関数last()を実装

--- monkey/evaluator/builtins.go
+++ monkey/evaluator/builtins.go
@@ -39,4 +39,22 @@ var builtins = map[string]*object.Builtin{
 			return NULL
 		},
 	},
+	"last": {
+		func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `last` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[len(arr.Elements)-1]
+			}
+
+			return NULL
+		},
+	},
 }
