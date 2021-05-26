d986537a7e36b7076afc5f3cfb9901a1e479f6be
mohira <shukatsudaison@gmail.com>
2021-05-26 11:27:45 +0900

    p.207: GREEN 組み込み関数rest()を実装

--- monkey/evaluator/builtins.go
+++ monkey/evaluator/builtins.go
@@ -50,8 +50,32 @@ var builtins = map[string]*object.Builtin{
 			}
 
 			arr := args[0].(*object.Array)
-			if len(arr.Elements) > 0 {
-				return arr.Elements[len(arr.Elements)-1]
+			length := len(arr.Elements)
+			if length > 0 {
+				return arr.Elements[length-1]
+			}
+
+			return NULL
+		},
+	},
+	"rest": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, arr.Elements[1:length])
+
+				return &object.Array{Elements: newElements}
 			}
 
 			return NULL
