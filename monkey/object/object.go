d27b698cc55235e0adf31ebf9d1a8e6aae87e14b
mohira <shukatsudaison@gmail.com>
2021-05-28 12:14:22 +0900

    p.220: GREEN object.Integer, object.Boolean, object.String でハッシュ値を生成できるようにした

--- monkey/object/object.go
+++ monkey/object/object.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"go-monkey-shakyo/monkey/ast"
+	"hash/fnv"
 	"strings"
 )
 
@@ -32,6 +33,9 @@ type Integer struct {
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
 
 type Boolean struct {
 	Value bool
@@ -39,6 +43,17 @@ type Boolean struct {
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: b.Type(), Value: value}
+}
 
 type NULL struct{}
 
@@ -91,6 +106,12 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
 
 type BuiltinFunction func(args ...Object) Object
 
@@ -120,4 +141,9 @@ func (ao *Array) Inspect() string {
 	out.WriteString("]")
 
 	return out.String()
+}
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
 }
