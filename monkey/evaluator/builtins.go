361f5bef0b7f180c5269ca0959cfd78651f617a2
mohira <shukatsudaison@gmail.com>
2021-05-28 13:01:46 +0900

    p.231: グランドフィナーレ！ 組み込み関数puts()を実装！

--- monkey/evaluator/builtins.go
+++ monkey/evaluator/builtins.go
@@ -102,4 +102,14 @@ var builtins = map[string]*object.Builtin{
 			return &object.Array{Elements: newElements}
 		},
 	},
+
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
+		},
+	},
 }
