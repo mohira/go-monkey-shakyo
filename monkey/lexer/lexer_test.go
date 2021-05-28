2e5f113048ee404793a84fd5c121aa237e7a105d
mohira <shukatsudaison@gmail.com>
2021-05-28 11:26:14 +0900

    p.211: RED `:` の字句解析のテストを追加

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -28,6 +28,7 @@ if (5 < 10) {
 "foobar"
 "foo bar"
 [1, 2];
+{"foo": "bar"}
 `
 
 	tests := []struct {
@@ -149,6 +150,13 @@ if (5 < 10) {
 		{token.INT, "2"},
 		{token.RBRACEKT, "]"},
 		{token.SEMICOLON, ";"},
+
+		// {"foo": "bar"}
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
 
 		{token.EOF, ""},
 	}
--- monkey/token/token.go
+++ monkey/token/token.go
@@ -32,6 +32,7 @@ const (
 	// デリミタ
 	COMMA     = ","
 	SEMICOLON = ";"
+	COLON     = ":"
 
 	// かっこ
 	LPAREN   = "("
